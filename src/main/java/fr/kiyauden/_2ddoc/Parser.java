b8eb0f145a7f09d210a77ba564ee39c10a9a35c7
Paul Petit <paul.petit76@gmail.com>
2023-02-05 18:30:05 +0100

    fix: Missing files from last commit

--- src/main/java/fr/kiyauden/_2ddoc/Parser.java
+++ src/main/java/fr/kiyauden/_2ddoc/Parser.java
@@ -1,26 +1,23 @@
 package fr.kiyauden._2ddoc;
 
+import java.security.cert.X509Certificate;
+import java.util.List;
+
 /**
  * Interface for a parser
- * <p>
- * Its job is to parse the data extracted from the 2D-Doc (always a string) to the corresponding java class
- *
- * @param <T> the returned type of this parser
  */
-interface Parser<T> {
-    /**
-     * Parses a data into the T type
-     *
-     * @param data the data to parse
-     * @return the data parsed into the T type
-     * @throws ParsingException when an error occurred while parsing a data
-     */
-    T parse(String data) throws ParsingException;
+public interface Parser {
 
     /**
-     * Returns the handled data format for this parser
+     * Parses a 2D-DOC
      *
-     * @return the handled format
+     * @param input        the input 2D-DOC string representation
+     * @param certificates the list of certificates to use for the signature verification
+     * @return an object representation of a 2D-DOC
+     * @throws IllegalArgumentException when the input is not a 2D-DOC
+     * @throws UnsupportedException     when the 2D-DOC version is not supported or the document is not supported
+     * @throws ParsingException         when any other error happens
      */
-    DataFormat getHandledFormat();
+    Parsed2DDoc parse(String input, List<X509Certificate> certificates) throws UnsupportedException, ParsingException;
+
 }
